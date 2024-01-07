bdb9454d19c09fa490f7ed4c8446b6b5265f9b68
Marcus Chui <marcus.chui.ca@icloud.com>
2024-01-07 04:20:55 -0500

    go back previous version because it's clearly the preferred way of doing this inside React

--- src/App.js
+++ src/App.js
@@ -63,23 +63,14 @@ export default function App() {
       </NavBar>
 
       <Main>
-        <Box element={<MovieList movies={movies} />} />
-        <Box
-          element={
-            <>
-              <WatchedSummary watched={watched} />
-              <WatchedMoviesList watched={watched} />
-            </>
-          }
-        />
-        {/*<Box>*/}
-        {/*  <MovieList movies={movies} />*/}
-        {/*</Box>*/}
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
 
-        {/*<Box>*/}
-        {/*  <WatchedSummary watched={watched} />*/}
-        {/*  <WatchedMoviesList watched={watched} />*/}
-        {/*</Box>*/}
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </Box>
       </Main>
     </>
   );
@@ -124,7 +115,7 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function Box({ element }) {
+function Box({ children }) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
@@ -132,7 +123,7 @@ function Box({ element }) {
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
-      {isOpen && element}
+      {isOpen && children}
     </div>
   );
 }
