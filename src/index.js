0d11017b824abdd4432e1752400ceb572697648a
Marcus Chui <marcus.chui.ca@icloud.com>
2024-01-07 05:54:02 -0500

    add StarRating component with own style.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import StartRating from "./StartRating";
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+  <React.StrictMode>
+    {/*<App/>*/}
+    <StartRating maxRating={5} />
+    <StartRating maxRating={10} />
+    <StartRating />
+  </React.StrictMode>,
 );
-
