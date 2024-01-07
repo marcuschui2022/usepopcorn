c90e646e1f70f781775a44f37f1a6c0eea85b308
Marcus Chui <marcus.chui.ca@icloud.com>
2024-01-07 04:17:33 -0500

    Passing elements as props ( alternative to children )
    * like react-router.
    * under the hood, inside React it should also be basically exactly the same.(viable pattern, in case need to pass in multiple elements)

--- src/App.js
+++ src/App.js
@@ -63,14 +63,23 @@ export default function App() {
       </NavBar>
 
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box element={<MovieList movies={movies} />} />
+        <Box
+          element={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList watched={watched} />
+            </>
+          }
+        />
+        {/*<Box>*/}
+        {/*  <MovieList movies={movies} />*/}
+        {/*</Box>*/}
 
-        <Box>
-          <WatchedSummary watched={watched} />
-          <WatchedMoviesList watched={watched} />
-        </Box>
+        {/*<Box>*/}
+        {/*  <WatchedSummary watched={watched} />*/}
+        {/*  <WatchedMoviesList watched={watched} />*/}
+        {/*</Box>*/}
       </Main>
     </>
   );
@@ -115,7 +124,7 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function Box({ children }) {
+function Box({ element }) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
@@ -123,7 +132,7 @@ function Box({ children }) {
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
-      {isOpen && children}
+      {isOpen && element}
     </div>
   );
 }
