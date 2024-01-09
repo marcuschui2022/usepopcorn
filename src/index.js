8090f0e49ebd00808edacc730cf9039742e6b199
Marcus Chui <marcus.chui.ca@icloud.com>
2024-01-09 03:53:43 -0500

    add more reusability with props to Star component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import StartRating from "./StartRating";
+
 // import App from './App';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StartRating color={"blue"} maxRating={20} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/*<App/>*/}
-    <StartRating maxRating={5} />
+    <StartRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StartRating
+      maxRating={10}
+      size={24}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>,
 );
