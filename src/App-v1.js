25a35ed9bab6df2196145d0dab3d9539dc16c2de
Marcus Chui <marcus.chui.ca@icloud.com>
2024-01-26 23:33:35 -0500

    proof not to fetch in rendering #side effect & infinite loop!

--- src/App-v1.js
+++ src/App-v1.js
@@ -50,14 +50,9 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "4286add4";
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-    .then((res) => res.json())
-    .then((data) => setMovies(data.Search));
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
