cc3a16877bc4f53a8fad0f251d46053ab340f2b3
Kwok Yuk Chui <kchui8@my.centennialcollege.ca>
2024-01-30 03:27:36 -0500

    fix duplicate ratting movie

--- src/App.js
+++ src/App.js
@@ -45,6 +45,21 @@ export default function App() {
   }
 
   function handleAddWatched(movie) {
+    // if (
+    //   watched.findIndex((existMovie) => existMovie.imdbID === movie.imdbID) > 0
+    // ) {
+    //   console.log(1);
+    //   console.log(watched);
+    //   setWatched((watched) => [
+    //     ...watched.filter((existMovie) => existMovie.imdbID !== movie.imdbID),
+    //     movie,
+    //   ]);
+    // } else {
+    //   console.log(2);
+    //   console.log(watched);
+    //   setWatched((watched) => [...watched, movie]);
+    // }
+
     setWatched((watched) => [...watched, movie]);
   }
 
@@ -115,6 +130,7 @@ export default function App() {
               selectedId={selectedId}
               onCloseMovie={handleCloseMovie}
               onAddWatched={handleAddWatched}
+              watched={watched}
             />
           ) : (
             <>
@@ -215,10 +231,15 @@ function Movie({ movie, onSelectMovie }) {
   );
 }
 
-function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
+function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
+
+  const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
+  const watchedUserRatting = watched.find(
+    (movie) => movie.imdbID === selectedId,
+  )?.userRating;
 
   const {
     Title: title,
@@ -288,15 +309,24 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
 
           <section>
             <div className="rating">
-              <StarRating
-                maxRating={10}
-                size={25}
-                onSetRating={setUserRating}
-              />
-              {userRating > 0 && (
-                <button className="btn-add" onClick={handleAdd}>
-                  + Add to list
-                </button>
+              {!isWatched ? (
+                <>
+                  <StarRating
+                    maxRating={10}
+                    size={25}
+                    onSetRating={setUserRating}
+                  />
+
+                  {userRating > 0 && (
+                    <button className="btn-add" onClick={handleAdd}>
+                      + Add to list
+                    </button>
+                  )}
+                </>
+              ) : (
+                <p>
+                  You rated with movie {watchedUserRatting} <span>⭐️</span>
+                </p>
               )}
             </div>
 
