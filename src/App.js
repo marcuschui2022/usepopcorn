21501c19f8deac9b544d9c1ea6dfa236a033eab4
Kwok Yuk Chui <kchui8@my.centennialcollege.ca>
2024-01-31 02:28:31 -0500

    implement better approach for prevent search movie many times a moment

--- src/App.js
+++ src/App.js
@@ -31,12 +31,15 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
             `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal },
           );
 
           if (!res.ok)
@@ -46,9 +49,12 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found");
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -60,14 +66,19 @@ export default function App() {
         return;
       }
 
-      const handler = setTimeout(() => {
-        fetchMovies();
-      }, 1000);
+      // const handler = setTimeout(() => {
+      //   fetchMovies();
+      // }, 1000);
+      //
+      // return () => {
+      //   clearTimeout(handler);
+      // };
 
-      return () => {
-        clearTimeout(handler);
+      fetchMovies();
+
+      return function () {
+        controller.abort();
       };
-      // fetchMovies();
     },
     [query],
   );
